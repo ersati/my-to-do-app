43c0aeb0dd2603e8a5a6248b5506f782b04edf27
ersati <piotrp92@gmail.com>
2021-05-17 20:15:44 +0100

    add styles to the projects

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 
 app.set('view engine', 'ejs');
-app.use(express.static("public"))
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use(express.static("public"))
 app.get('/', (req,res) => {
     res.render('list')
 })
