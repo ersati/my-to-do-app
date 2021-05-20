5b47b4a36cc3fdf5e737f39f11bd5a93eb2e3f8f
ersati <piotrp92@gmail.com>
2021-05-20 15:47:17 +0100

    add custom params to the projects

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(express.static("public"))
 
 const arr = []
 const list = []
+let param = '';
 app.get('/', (req,res) => {
     res.render('list',{
         people: arr,
@@ -43,10 +44,20 @@ app.get ('/fandf', (req,res) => {
     res.render('fandf')
 })
 
+
+app.get('/:paramName', (req, res) => {
+    const paramName = req.params.paramName;
+    console.log(paramName)
+    res.render('own', {
+        title: paramName
+    })
+})
+
 app.post('/', (req,res) =>{
     const valueInput = req.body.name2;
     const valueInput1 = req.body.name1;
-    
+    const random = req.params
+    console.log(random)
     arr.push(valueInput);
     console.log(arr)
     console.log(req.body)
