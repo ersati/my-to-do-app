b4ac297149c6dde59dd6f20e737e956e61b8191d
ersati <piotrp92@gmail.com>
2021-11-26 01:57:28 +0000

    add validation to password

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,37 @@
 const user = document.querySelector('input[name="username"]');
 const pass = document.querySelector('input[name="password"]');
-if(user){
+if (user) {
+    function validateUser() {
+        const isInvalidElExists = document.querySelector('.invalid__user');
 
-
-function validateUser () {
-    const isInvalidElExists = document.querySelector('.invalid');
-    function getSecondPart(str, char) {
-        return str.split(char)[1];
-    }
-
-   if(!user.value.includes('@')){
-       console.log(isInvalidElExists)
-        if(isInvalidElExists === null){
-            user.insertAdjacentHTML('afterend', '<p class="invalid">Please insert correct email</p>')
-        } 
-
-   }else {
-       const partAfterAt = getSecondPart(user.value, '@')
-        const isContainsDot  = getSecondPart(partAfterAt, '.')
-        if(partAfterAt && isContainsDot){
-            isInvalidElExists.remove()
+        function getSecondPart(str, char) {
+            return str.split(char)[1];
         }
+
+        if (!user.value.includes('@')) {
+            console.log(isInvalidElExists)
+            if (isInvalidElExists === null) {
+                user.insertAdjacentHTML('afterend', '<p class="invalid__user">Please insert correct email</p>')
+            }
+
+        } else {
+            const partAfterAt = getSecondPart(user.value, '@')
+            const isContainsDot = getSecondPart(partAfterAt, '.')
+            if (partAfterAt && isContainsDot) {
+                isInvalidElExists.remove()
+            }
+        }
+    }
+function validPassword () {
+    const isInvalidElExists = document.querySelector('.invalid__password');
+    const regex = /^(?=.*?[A-Z])(?=.*?[a-z]).{8,20}$/
+    console.log('inside', pass.value.length, pass.value.match(regex))
+    if(!pass.value.match(regex)) {
+        pass.insertAdjacentHTML('afterend', '<p class="invalid__user">Password its to short, please put at list 8 characters, Capital and lowercase letter</p>')
+    }
+   
 }
-}
-user.addEventListener('change', validateUser)
+
+    user.addEventListener('change', validateUser)
+    pass.addEventListener('change', validPassword)
 }
