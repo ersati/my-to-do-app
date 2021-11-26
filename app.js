5cd7e0116032c45a0d6b41e0a6a8a6dbd1bcb169
ersati <piotrp92@gmail.com>
2021-11-26 21:21:48 +0000

    update google callback to authoarization

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/hexagon",
+        callbackURL: "https://hex-task-tanager.herokuapp.com/auth/google/hexagon",
         userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
     },
     function (accessToken, refreshToken, profile, cb) {
